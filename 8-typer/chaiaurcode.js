0bdafcd658211b43c494334913eae8b574f365a9
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-01-02 23:10:09 +0530

    project-8

--- 8-typer/chaiaurcode.js
+++ 8-typer/chaiaurcode.js
@@ -1,8 +1,30 @@
 const typedTextSpan = document.querySelector('.typed-text');
 const cursor = document.querySelector('.cursor');
 
-const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
+const words = ['Love', 'Jhakaas', 'Masstt', 'Dhinchak', 'Weird'];
+let index = 0;
 
-function type() {}
+function delay(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration));
+}
 
-function erase() {}
+async function type() {
+  for(let ch of words[index]) {
+    typedTextSpan.innerText += ch;
+    await delay(350);
+  }
+  await delay(350);
+  erase();
+}
+
+async function erase() {
+  while(typedTextSpan.innerText.length > 0) {
+    typedTextSpan.innerText = typedTextSpan.innerText.slice(0, -1);
+    await delay(350);
+  }
+  // using Modulus(div. remainder) in the following way, the code cycles through the words in the array, one by one. After the last word, it goes back to the first word.
+  index = (index + 1) % words.length;
+  type();
+}
+
+type(); // start typing
