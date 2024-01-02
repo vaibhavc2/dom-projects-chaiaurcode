6bc9b7eb6028edb46c07c0e72d2c51e06ab022d0
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-01-02 19:19:35 +0530

    project-7

--- 7-scroll/chaiaurcode.js
+++ 7-scroll/chaiaurcode.js
@@ -1,1 +1,6 @@
-// use indicator width
+window.addEventListener('scroll', function() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("scrollIndicator").style.width = scrolled + "%";
+});
