3c84fd74bba1ade73e568facffc4087bd8ec39c9
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-01-04 16:50:09 +0530

    project-9

--- 9-mouseCircle/chaiaurcode.js
+++ 9-mouseCircle/chaiaurcode.js
@@ -1,4 +1,5 @@
 const cursor = document.querySelector('.cursor');
+
 // an array of 10 colors in hex value
 const colors = [
   '#FF6633',
@@ -12,4 +13,25 @@ const colors = [
   '#99FF99',
   '#B34D4D',
 ];
+
 // add circle to cursor and change it's color as cursor moves on the screen. Pick color from these array
+
+let colorIndex = 0;
+const circle = document.querySelector('.cursor');
+let prevPositionX = 0;
+let prevPositionY = 0;
+
+document.addEventListener('mousemove', function(e) {
+  // update circle position
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
+  
+  // change color
+  if((Math.abs(prevPositionX - e.pageX) < 20) || (Math.abs(e.pageY - prevPositionY) < 20)) return;
+  colorIndex = (colorIndex + 1) % colors.length;
+  circle.style.backgroundColor = colors[colorIndex];
+
+  // update prev positions
+  prevPositionX = e.pageX;
+  prevPositionY = e.pageY;
+});
