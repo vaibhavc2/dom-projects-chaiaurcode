9d177ef3b600ce9adf3e95be26d266b3138ba379
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-01-05 15:13:18 +0530

    project-12

--- 12-randomImage/chaiaurcode.js
+++ 12-randomImage/chaiaurcode.js
@@ -1,3 +1,12 @@
 const baseURL = 'https://source.unsplash.com/all/300x300';
 
 // this url gives an image. Use this and NO API calls
+const image = document.createElement("img");
+image.src = baseURL;
+image.alt = "random img";
+document.querySelector(".content").appendChild(image);
+
+// refresh and get random img
+setInterval(() => {
+  window.location.reload();
+}, 2000)
