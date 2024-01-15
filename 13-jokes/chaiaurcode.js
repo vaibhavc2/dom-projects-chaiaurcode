9fa87d1cb7fcb80da07f1f73c0c8653f4a17ad63
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-01-15 20:20:39 +0530

    handle race conditions in project 13

--- 13-jokes/chaiaurcode.js
+++ 13-jokes/chaiaurcode.js
@@ -5,26 +5,33 @@ let jokeData;
 const setJokeData = (data) => jokeField.innerHTML = data.value;
 
 // handle this end point with XMLHttpRequest
+// also handle the case of race condition
 const xhr = new XMLHttpRequest();
 const fetchRandomJoke_XHR = () => {
+  xhr.abort();
   xhr.open('GET', url);
   xhr.send();
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4) {
-      setJokeData(JSON.parse(this.responseText))
+      setJokeData(JSON.parse(xhr.responseText))
     }
   }
 }
 
 // handle this end point with promises
+let lastController;
 
 const fetchRandomJoke = async () => {
   try {
+    if(lastController) lastController.abort();
+    const currentController = new AbortController();
+    const signal = currentController.signal;
+    lastController = currentController;
     jokeData = await new Promise((resolve, reject) => {
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => resolve(data))
-        .catch((err) => reject(err));
+      fetch(url, { signal })
+      .then((res) => res.json())
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
     });
     setJokeData(jokeData);
   } catch(error) {
@@ -37,4 +44,3 @@ document.getElementById("getJoke-fetch").addEventListener("click", fetchRandomJo
 document.getElementById("getJoke-xhr").addEventListener("click", fetchRandomJoke_XHR);
 
 
-// handle the case of race condition
