9a9eb736fefa1755415f32e17c261ea0a4410de7
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-01-15 23:16:26 +0530

    project 15

--- 15-crudDom/chaiaurcode.js
+++ 15-crudDom/chaiaurcode.js
@@ -4,6 +4,21 @@ const year = document.getElementById('year');
 const bookList = document.getElementById('book-list');
 const btn = document.querySelector('.btn');
 
+const appendToBook = (parent, content) => {
+  const element = document.createElement("div");
+  element.innerHTML = content;
+  parent.appendChild(element);
+};
+
 btn.addEventListener('click', function (e) {
+  e.preventDefault();
+
   //work on CRUD operation of DOM
+  const book = document.createElement("section");
+
+  appendToBook(book, title.value);
+  appendToBook(book, author.value);
+  appendToBook(book, year.value);
+
+  bookList.appendChild(book);
 });
